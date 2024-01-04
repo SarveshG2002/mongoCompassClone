0af591f33847f0c4c90a8525a863dbbff3a91856
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-01-04 21:46:59 +0530

    ejs setup done

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
-// app.js
+// server/app.js
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+// Serve static files from the 'public' folder (if needed)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Render index.ejs when a request is made to the root route
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.render('index');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
