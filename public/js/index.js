20a9226df98b35eaf7544bfdb417914f721decee
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-01-08 23:21:33 +0530

    designed update on maimn panel

--- public/js/index.js
+++ public/js/index.js
@@ -1,133 +1,120 @@
-function setDatabase() {
+var url=(new URL(window.location.href)).origin+"/";
+
+function showCollectionInSideBar(id, selfele) {
+    // console.log(id)
+    let ele = document.getElementById(`collectionDiv${id}`);
+    // Make an AJAX request using jQuery
+    if (ele.style.display == "") {
+        ele.style.display = "none";
+        selfele.innerHTML = "chevron_right";
+    } else {
+        $.ajax({
+            url: `${url}getCollectionByDatabaseSidebarPage`,
+            type: 'POST',
+            data: { dbname: id }, // This will send data in the request body
+            success: function (data) {
+                // console.log(data);
+                document.getElementById(`collectionDiv${id}`).innerHTML = data;
+                ele.style.display = "";
+                selfele.innerHTML = "expand_more";
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', status, error);
+            }
+        });
+
+    }
 
 }
 
-function showCollectionInSideBar(id, selfele) {
-  // console.log(id)
-  let ele = document.getElementById(`collectionDiv${id}`);
-  // Make an AJAX request using jQuery
-  if (ele.style.display == "") {
-    ele.style.display = "none";
-    selfele.innerHTML = "chevron_right";
-  } else {
-    $.ajax({
-      url: 'http://localhost:3000/getCollectionByDatabaseSidebarPage',
-      type: 'POST',
-      data: { dbname: id }, // This will send data in the request body
-      success: function (data) {
-        // console.log(data);
-        document.getElementById(`collectionDiv${id}`).innerHTML = data;
-        ele.style.display = "";
-        selfele.innerHTML = "expand_more";
-      },
-      error: function (xhr, status, error) {
-        console.error('Error:', status, error);
-      }
-    });
+function showCollectioninPanel(dbname){
 
-  }
-
-
-
-  // ele.style.display = (ele.style.display == "") ? "none" : "";
-  //  = (ele.style.display != "") ? "chevron_right" : "expand_more";
 }
 
 
 
 $(document).ready(function () {
-  // Attach a click event to the button
+    // Attach a click event to the button
 
-  // Make an AJAX request using jQuery
-  $('#fetchDatabase').click(function () {
-    $.ajax({
-      url: 'http://localhost:3000/getAllDatabaseListSidebarPage',  // Replace with your server-side endpoint
-      type: 'GET',
-      success: function (data) {
-        // Handle the successful response
-        $('#allDatabases').html(data);
-        // console.log(data);
-        // console.log('database fetch successfulyy');
-      },
-      error: function (xhr, status, error) {
-        // Handle errors
-        console.error('Error:', status, error);
-      }
-    });
+    // Make an AJAX request using jQuery
+    $('#fetchDatabase').click(function () {
+        $.ajax({
+            url: `${url}getAllDatabaseListSidebarPage`,  // Replace with your server-side endpoint
+            type: 'GET',
+            success: function (data) {
+                // Handle the successful response
+                $('#allDatabases').html(data);
+                // console.log(data);
+                // console.log('database fetch successfulyy');
+            },
+            error: function (xhr, status, error) {
+                // Handle errors
+                console.error('Error:', status, error);
+            }
+        });
 
-  })
+    })
 
-  $('#createDatabase').click(function () {
-    let databaseName=$("#newDatabaseName").val()
-    let collectionName=$("#newCollectionName").val()
-    $.ajax({
-      url: 'http://localhost:3000/createDatabaseAndCollection',  // Replace with your server-side endpoint
-      type: 'POST',
-      data: {dbname:databaseName,collectionname:collectionName},
-      success: function (data) {
-        // Handle the successful response
-        // $('#allDatabases').html(data);
-        // console.log(data);
-        // console.log('database fetch successfulyy');
-        modal.css('display', 'none');
-        overlay.css('display', 'none');
-        $('#fetchDatabase').click();
-      },
-      error: function (xhr, status, error) {
-        // Handle errors
-        console.error('Error:', status, error);
-      }
-    });
+    $('#createDatabase').click(function () {
+        let databaseName = $("#newDatabaseName").val()
+        let collectionName = $("#newCollectionName").val()
+        $.ajax({
+            url: `${url}createDatabaseAndCollection`,  // Replace with your server-side endpoint
+            type: 'POST',
+            data: { dbname: databaseName, collectionname: collectionName },
+            success: function (data) {
+                // Handle the successful response
+                // $('#allDatabases').html(data);
+                // console.log(data);
+                // console.log('database fetch successfulyy');
+                modal.css('display', 'none');
+                overlay.css('display', 'none');
+                $('#fetchDatabase').click();
+            },
+            error: function (xhr, status, error) {
+                // Handle errors
+                console.error('Error:', status, error);
+            }
+        });
 
-  })
+    })
 
-  document.getElementById('fetchDatabase').click();
+    document.getElementById('fetchDatabase').click();
     var openModalBtn = $('#openModalBtn');
     var closeModalBtn = $('#closeModalBtn');
 
     var modal = $('#myModal');
     var overlay = $('#overlay');
-    
+
     // var modalHead= $('#modalHead')
 
     openModalBtn.on('click', function () {
-      document.getElementById('modalHead').innerHTML="Create Database";
-      modal.css('display', 'block');
-      overlay.css('display', 'block');
-      
+        document.getElementById('modalHead').innerHTML = "Create Database";
+        modal.css('display', 'block');
+        overlay.css('display', 'block');
+
     });
 
     closeModalBtn.on('click', function () {
-      modal.css('display', 'none');
-      overlay.css('display', 'none');
+        modal.css('display', 'none');
+        overlay.css('display', 'none');
     });
 
     // Close the modal if the user clicks outside the modal content
     $(window).on('click', function (event) {
-      if (event.target === modal[0]) {
-        modal.css('display', 'none');
-        overlay.css('display', 'none');
-      }
+        if (event.target === modal[0]) {
+            modal.css('display', 'none');
+            overlay.css('display', 'none');
+        }
     });
 
-    
+});
 
-  });
-
-  function openCollectionModal(database){
+function openCollectionModal(database) {
     var modal = $('#myModal');
     var overlay = $('#overlay');
-      document.getElementById('modalHead').innerHTML="Create Collection";
-      $("#newDatabaseName").val(database)
-      modal.css('display', 'block');
-      overlay.css('display', 'block');
-    }
-
-  
-
-  
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-});
+    document.getElementById('modalHead').innerHTML = "Create Collection";
+    $("#newDatabaseName").val(database)
+    modal.css('display', 'block');
+    overlay.css('display', 'block');
+}
